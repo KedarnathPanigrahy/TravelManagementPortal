10099ff689d1895e8a54ac1612c298d780c9f5e9
Punam Panigrahi <punampanigrahi3@gmail.com>
2021-01-15 22:30:53 +0530

    Updating the Notification Service

--- notification-service/src/main/java/com/kd/microservices/tmp/ns/controller/NotifyController.java
+++ notification-service/src/main/java/com/kd/microservices/tmp/ns/controller/NotifyController.java
@@ -2,19 +2,25 @@ package com.kd.microservices.tmp.ns.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kd.microservices.tmp.ns.dto.CustomerDto;
 import com.kd.microservices.tmp.ns.service.NotifyService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/notify")
+@Slf4j
 public class NotifyController {
 	@Autowired
 	private NotifyService service;
 
 	@GetMapping("/notify-user")
-	public void notifyUser() {
-		service.notifyUser("Kedar", "Kedarnath.Panigrahy@Lumen.com");
+	public void notifyUser(@RequestBody CustomerDto customer) {
+		log.info("NotifyController - notifyUser");
+		service.notifyUser(customer.getFirstName() + " " + customer.getLastName(), customer.getEmailId());
 	}
 }
--- notification-service/src/main/java/com/kd/microservices/tmp/ns/service/NotifyService.java
+++ notification-service/src/main/java/com/kd/microservices/tmp/ns/service/NotifyService.java
@@ -1,78 +1,59 @@
 package com.kd.microservices.tmp.ns.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Date;
-import java.util.Properties;
-
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileDataSource;
-import javax.mail.BodyPart;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-
 @Service
 @Slf4j
 public class NotifyService {
-	@Autowired
-	private JavaMailSender mailSender;
 
 	public void notifyUser(String user, String email) {
 		log.info("Started NotifyService - notifyUser");
-//		SimpleMailMessage message = new SimpleMailMessage();
-//		message.setFrom("techiekedar90@gmail.com");
-//		message.setTo(email);
-//		message.setText("Test Text");
-//		message.setSubject("Test Body");
-//
-//		mailSender.send(message);
-
-		String smtpHostServer = "smtp.example.com";
-		Properties props = System.getProperties();
-		props.put("mail.smtp.host", smtpHostServer);
-
-		Session session = Session.getInstance(props, null);
-		sendEmail(session, email, "SimpleEmail Testing Subject", "SimpleEmail Testing Body");
-		log.info("Mail Sent to : " + email);
-	}
-
-	public static void sendEmail(Session session, String toEmail, String subject, String body) {
 		try {
-			MimeMessage msg = new MimeMessage(session);
-			msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
-			msg.addHeader("format", "flowed");
-			msg.addHeader("Content-Transfer-Encoding", "8bit");
+			Properties props = new Properties();
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "587");
 
-			msg.setFrom(new InternetAddress("no_reply@example.com", "NoReply-JD"));
+			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("test@gmail.com", "oecjzzaajbjfbbbi");
+				}
+			});
+			
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress("test@gmail.com", false));
 
-			msg.setReplyTo(InternetAddress.parse("no_reply@example.com", false));
-
-			msg.setSubject(subject, "UTF-8");
-
-			msg.setText(body, "UTF-8");
-
+			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+			msg.setSubject("Sample Email");
+			msg.setContent("Hey "+ user + ", this is the sample email.", "text/html");
 			msg.setSentDate(new Date());
 
-			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
-			System.out.println("Message is ready");
+//			MimeBodyPart messageBodyPart = new MimeBodyPart();
+//			messageBodyPart.setContent("SimpleEmail", "text/html");
+//			Multipart multipart = new MimeMultipart();
+//			multipart.addBodyPart(messageBodyPart);
+//			MimeBodyPart attachPart = new MimeBodyPart();
+//			attachPart.attachFile("/var/tmp/image19.png");
+//			multipart.addBodyPart(attachPart);
+//			msg.setContent(multipart);
+			
 			Transport.send(msg);
-
-			System.out.println("EMail Sent Successfully!!");
+			log.info("Mail Sent to : " + email);
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error("Error at NotifyService - notifyUser : " + e.getMessage());
 		}
 	}
 }
